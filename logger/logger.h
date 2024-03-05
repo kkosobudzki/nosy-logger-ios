571136ad1f2f68d8d4e26836b6adddf5a3aeade3
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-03-05 15:54:07 +0100

    chore: add base structure

---
+++ logger/logger.h
@@ -0,0 +1,18 @@
+//
+//  logger.h
+//  logger
+//
+//  Created by Krzysztof Kosobudzki on 05/03/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for logger.
+FOUNDATION_EXPORT double loggerVersionNumber;
+
+//! Project version string for logger.
+FOUNDATION_EXPORT const unsigned char loggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <logger/PublicHeader.h>
+
+
