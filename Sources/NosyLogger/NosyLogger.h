3fbeead1faac5d01ded17d50875724113e39ae9a
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-03-05 17:49:14 +0100

    chore: initial commit

--- Sources/NosyLogger/NosyLogger.h
+++ Sources/NosyLogger/NosyLogger.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double loggerVersionNumber;
 FOUNDATION_EXPORT const unsigned char loggerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <logger/PublicHeader.h>
-
-
---
+++ Sources/NosyLogger/NosyLogger.swift
@@ -0,0 +1,31 @@
+//
+//  NosyLogger.swift
+//  NosyLogger
+//
+//  Created by Krzysztof Kosobudzki on 05/03/2024.
+//
+
+import Foundation
+
+public class NosyLogger : NSObject {
+    
+    public func start(apiKey: String) {
+        print("TODO initialize with api key: \(apiKey)")
+    }
+    
+    public func debug(message: String) {
+        print("TODO debug: \(message)")
+    }
+    
+    public func info(message: String) {
+        print("TODO info: \(message)")
+    }
+    
+    public func warning(message: String) {
+        print("TODO warning: \(message)")
+    }
+    
+    public func error(message: String) {
+        print("TODO error: \(message)")
+    }
+}
