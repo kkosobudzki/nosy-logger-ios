43d1fbb8161fbaa4e4343cb1ce1526e3b63ad620
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-03-06 13:18:43 +0100

    chore: make methods available in objc

--- Sources/NosyLogger/NosyLogger.swift
+++ Sources/NosyLogger/NosyLogger.swift
@@ -13,6 +13,7 @@ public class NosyLogger : NSObject {
     private var collector: Collector?
     private var encryptor: Encryptor?
     
+    @objc
     public func start(apiKey: String) async {
         do {
             self.collector = try Collector(apiKey: apiKey)
@@ -25,18 +26,22 @@ public class NosyLogger : NSObject {
         }
     }
     
+    @objc
     public func debug(_ message: String) {
         print("TODO debug: \(message)")
     }
     
+    @objc
     public func info(_ message: String) {
         print("TODO info: \(message)")
     }
     
+    @objc
     public func warning(_ message: String) {
         print("TODO warning: \(message)")
     }
     
+    @objc
     public func error(_ message: String) {
         print("TODO error: \(message)")
     }
