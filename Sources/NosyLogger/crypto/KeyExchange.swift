b007c5026433aa5958ab720d30aa375549b6a810
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-03-08 12:45:39 +0100

    chore: add loggging

--- Sources/NosyLogger/crypto/KeyExchange.swift
+++ Sources/NosyLogger/crypto/KeyExchange.swift
@@ -20,9 +20,12 @@ class KeyExchange {
         let privateKey = P256.KeyAgreement.PrivateKey()
         
         self.publicKey = encodePublicKey(publicKey: privateKey.publicKey)
+        print("NosyLogger :: KeyExchange :: deriveSharedSecret: encoded public key \(publicKey)")
         
         let remotePublicKey = try decodePublicKey(publicKey: otherPublicKey)
+        print("NosyLogger :: KeyExchange :: deriveSharedSecret: decoded other public key \(remotePublicKey)")
         let sharedSecret = try privateKey.sharedSecretFromKeyAgreement(with: remotePublicKey)
+        print("NosyLogger :: KeyExchange :: deriveSharedSecret: aligned shared secret \(sharedSecret)")
         
         return sharedSecret.withUnsafeBytes { SymmetricKey(data: $0) }
     }
